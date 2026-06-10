2561230ec850e7c7bcfd87720c2960fc14bfb6f3
keletso <keletsomonyamane.dev@gmail.com>
2026-06-10 23:43:31 +0200

    chore: Add new files and delete old ones

--- src/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-in-production")
-    DATABASE = os.environ.get("DATABASE_URL", "tasks.db")
-    JWT_EXPIRATION_HOURS = 24
